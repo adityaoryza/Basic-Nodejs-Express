5e97538c2d31cdb90085ddbf111e4b52347834a1
adityaoryza <oryza429@gmail.com>
2023-04-19 22:13:47 +0700

    create an api to send json file

--- myApp.js
+++ myApp.js
@@ -5,9 +5,16 @@ absolutePath = __dirname + "/views/index.html";
 // Assets at the /public route using midleware
 app.use("/public", express.static(__dirname + "/public"));
 
+// main routes
 app.get("/", function (req, res) {
   //   res.send("Hello Express");
   res.sendFile(absolutePath);
 });
 
+// json routes
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
+});
 module.exports = app;
