82ded72d46ffcf5990f917cda6b7fc5e5bf88290
adityaoryza <oryza429@gmail.com>
2023-04-19 21:59:33 +0700

    using midleware and express.static serve public folder

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require("express");
 let app = express();
 absolutePath = __dirname + "/views/index.html";
 
+// Assets at the /public route using midleware
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   //   res.send("Hello Express");
   res.sendFile(absolutePath);
