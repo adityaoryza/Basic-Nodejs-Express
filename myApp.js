ac14bd0d14367858d1856b63f8516702214532a0
adityaoryza <oryza429@gmail.com>
2023-04-19 23:17:56 +0700

    Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -1,20 +1,36 @@
 let express = require("express");
 let app = express();
+require("dotenv").config();
 absolutePath = __dirname + "/views/index.html";
 
 // Assets at the /public route using midleware
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(function middleware(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  // Do something
+  // Call the next function in line:
+  next();
+});
+
 // main routes
 app.get("/", function (req, res) {
-  //   res.send("Hello Express");
   res.sendFile(absolutePath);
 });
 
-// json routes
+// // json routes
+// app.get("/json", (req, res) => {
+//   res.json({
+//     message: "Hello json",
+//   });
+// });
+
 app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
 });
+
 module.exports = app;
