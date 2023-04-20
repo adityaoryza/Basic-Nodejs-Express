2c984fb7d33db99683c6138b26cf8fce1bc80d56
adityaoryza <oryza429@gmail.com>
2023-04-20 20:45:15 +0700

    complete the course section 2

--- myApp.js
+++ myApp.js
@@ -1,15 +1,17 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 require("dotenv").config();
+const bodyParser = require("body-parser");
+
 absolutePath = __dirname + "/views/index.html";
 
 // Assets at the /public route using midleware
 app.use("/public", express.static(__dirname + "/public"));
-
+// use bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+// middleware usage example
 app.use(function middleware(req, res, next) {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
-  // Do something
-  // Call the next function in line:
   next();
 });
 
@@ -33,6 +35,7 @@ app.get("/json", (req, res) => {
   }
 });
 
+// chain midleware to create a time server
 app.get(
   "/now",
   (req, res, next) => {
@@ -45,5 +48,37 @@ app.get(
     });
   }
 );
+
+// Get Route Parameter Input from the Client
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word,
+  });
+});
+
+// Get Query Parameter Input from the Client
+app.get("/name", function (req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`,
+  });
+});
+
+// Get Data from POST Requests
+app.post("/name", function (req, res) {
+  var firstName = req.body.first;
+  var lastName = req.body.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.body;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`,
+  });
+});
 
 module.exports = app;
